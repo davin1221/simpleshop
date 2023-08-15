9c2b9139b47374a7111d90a7318bc438facf9aee
Davin <kdavin1221@gmail.com>
2023-08-16 01:13:05 +0900

    리액트실습- 쇼핑몰

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+// img
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Simple Shop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">About</Nav.Link>
+            <Nav.Link href="#pricing">Notice</Nav.Link>
+
+            <NavDropdown title="Product" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Necklace</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Earing</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Bracelet</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    <section className="main-bg">
+    </section>
+
+    <section className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <img src={process.env.PUBLIC_URL + "/imgs/necklace.webp"} width={"100%"}/>
+          <h5>NECKLACE</h5>
+        </div>
+        <div className="col-md-4">
+          <img src={process.env.PUBLIC_URL + "/imgs/earing.webp"} width={"100%"}/>
+          <h5>EARING</h5>
+        </div>
+        <div className="col-md-4">
+          <img src={process.env.PUBLIC_URL + "/imgs/bracelet.webp"} width={"100%"}/>
+          <h5>BRACELET</h5>
+        </div>
+      </div>
+    </section>
+
     </div>
   );
 }
